35ef0fa145e8261126ce270ee8a1f8afb8c31ca8
felps777@hotmail.com <felps777@hotmail.com>
2022-11-04 01:49:55 +0100

    Initial Commit

---
+++ src/components/Post.js
@@ -0,0 +1,14 @@
+//import React, { useState } from "react";
+
+export const Post = (title, text) => {
+    return {
+        title: title,
+        postText: text,
+        author: null
+    }
+
+    //const[title, setTitle] = useState("");
+    //const[postText, setPostText] = useState("");
+    //const[author, setAuthor] = useState({ });
+
+};
